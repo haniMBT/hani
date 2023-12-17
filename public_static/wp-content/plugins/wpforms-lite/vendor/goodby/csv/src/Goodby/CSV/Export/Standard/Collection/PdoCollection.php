3b3cba37e12b11bbeb4f95fb1e5a67839d610f20
haniMBT <abdelghaani16@gmail.com>
2023-12-17 15:21:20 +0100

    seconde commit

---
+++ public_static/wp-content/plugins/wpforms-lite/vendor/goodby/csv/src/Goodby/CSV/Export/Standard/Collection/PdoCollection.php
@@ -0,0 +1,105 @@
+<br>
+<font size="1"><table class="xdebug-error xe-deprecated" dir="ltr" border="1" cellspacing="0" cellpadding="1">
+<tr><th align="left" bgcolor="#f57900" colspan="5">
+<span style="background-color: #cc0000; color: #fce94f; font-size: x-large;">( ! )</span> Deprecated: Return type of Goodby\CSV\Export\Standard\Collection\PdoCollection::current() should either be compatible with Iterator::current(): mixed, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice in C:\wamp86\www\wordpress\wp-content\plugins\wpforms-lite\vendor\goodby\csv\src\Goodby\CSV\Export\Standard\Collection\PdoCollection.php on line <i>32</i>
+</th></tr>
+<tr><th align="left" bgcolor="#e9b96e" colspan="5">Call Stack</th></tr>
+<tr>
+<th align="center" bgcolor="#eeeeec">#</th>
+<th align="left" bgcolor="#eeeeec">Time</th>
+<th align="left" bgcolor="#eeeeec">Memory</th>
+<th align="left" bgcolor="#eeeeec">Function</th>
+<th align="left" bgcolor="#eeeeec">Location</th>
+</tr>
+<tr>
+<td bgcolor="#eeeeec" align="center">1</td>
+<td bgcolor="#eeeeec" align="center">0.0013</td>
+<td bgcolor="#eeeeec" align="right">364040</td>
+<td bgcolor="#eeeeec">{main}(  )</td>
+<td title="C:\wamp86\www\wordpress\wp-content\plugins\wpforms-lite\vendor\goodby\csv\src\Goodby\CSV\Export\Standard\Collection\PdoCollection.php" bgcolor="#eeeeec">...\PdoCollection.php<b>:</b>0</td>
+</tr>
+</table></font>
+<br>
+<font size="1"><table class="xdebug-error xe-deprecated" dir="ltr" border="1" cellspacing="0" cellpadding="1">
+<tr><th align="left" bgcolor="#f57900" colspan="5">
+<span style="background-color: #cc0000; color: #fce94f; font-size: x-large;">( ! )</span> Deprecated: Return type of Goodby\CSV\Export\Standard\Collection\PdoCollection::next() should either be compatible with Iterator::next(): void, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice in C:\wamp86\www\wordpress\wp-content\plugins\wpforms-lite\vendor\goodby\csv\src\Goodby\CSV\Export\Standard\Collection\PdoCollection.php on line <i>43</i>
+</th></tr>
+<tr><th align="left" bgcolor="#e9b96e" colspan="5">Call Stack</th></tr>
+<tr>
+<th align="center" bgcolor="#eeeeec">#</th>
+<th align="left" bgcolor="#eeeeec">Time</th>
+<th align="left" bgcolor="#eeeeec">Memory</th>
+<th align="left" bgcolor="#eeeeec">Function</th>
+<th align="left" bgcolor="#eeeeec">Location</th>
+</tr>
+<tr>
+<td bgcolor="#eeeeec" align="center">1</td>
+<td bgcolor="#eeeeec" align="center">0.0013</td>
+<td bgcolor="#eeeeec" align="right">364040</td>
+<td bgcolor="#eeeeec">{main}(  )</td>
+<td title="C:\wamp86\www\wordpress\wp-content\plugins\wpforms-lite\vendor\goodby\csv\src\Goodby\CSV\Export\Standard\Collection\PdoCollection.php" bgcolor="#eeeeec">...\PdoCollection.php<b>:</b>0</td>
+</tr>
+</table></font>
+<br>
+<font size="1"><table class="xdebug-error xe-deprecated" dir="ltr" border="1" cellspacing="0" cellpadding="1">
+<tr><th align="left" bgcolor="#f57900" colspan="5">
+<span style="background-color: #cc0000; color: #fce94f; font-size: x-large;">( ! )</span> Deprecated: Return type of Goodby\CSV\Export\Standard\Collection\PdoCollection::key() should either be compatible with Iterator::key(): mixed, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice in C:\wamp86\www\wordpress\wp-content\plugins\wpforms-lite\vendor\goodby\csv\src\Goodby\CSV\Export\Standard\Collection\PdoCollection.php on line <i>54</i>
+</th></tr>
+<tr><th align="left" bgcolor="#e9b96e" colspan="5">Call Stack</th></tr>
+<tr>
+<th align="center" bgcolor="#eeeeec">#</th>
+<th align="left" bgcolor="#eeeeec">Time</th>
+<th align="left" bgcolor="#eeeeec">Memory</th>
+<th align="left" bgcolor="#eeeeec">Function</th>
+<th align="left" bgcolor="#eeeeec">Location</th>
+</tr>
+<tr>
+<td bgcolor="#eeeeec" align="center">1</td>
+<td bgcolor="#eeeeec" align="center">0.0013</td>
+<td bgcolor="#eeeeec" align="right">364040</td>
+<td bgcolor="#eeeeec">{main}(  )</td>
+<td title="C:\wamp86\www\wordpress\wp-content\plugins\wpforms-lite\vendor\goodby\csv\src\Goodby\CSV\Export\Standard\Collection\PdoCollection.php" bgcolor="#eeeeec">...\PdoCollection.php<b>:</b>0</td>
+</tr>
+</table></font>
+<br>
+<font size="1"><table class="xdebug-error xe-deprecated" dir="ltr" border="1" cellspacing="0" cellpadding="1">
+<tr><th align="left" bgcolor="#f57900" colspan="5">
+<span style="background-color: #cc0000; color: #fce94f; font-size: x-large;">( ! )</span> Deprecated: Return type of Goodby\CSV\Export\Standard\Collection\PdoCollection::valid() should either be compatible with Iterator::valid(): bool, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice in C:\wamp86\www\wordpress\wp-content\plugins\wpforms-lite\vendor\goodby\csv\src\Goodby\CSV\Export\Standard\Collection\PdoCollection.php on line <i>66</i>
+</th></tr>
+<tr><th align="left" bgcolor="#e9b96e" colspan="5">Call Stack</th></tr>
+<tr>
+<th align="center" bgcolor="#eeeeec">#</th>
+<th align="left" bgcolor="#eeeeec">Time</th>
+<th align="left" bgcolor="#eeeeec">Memory</th>
+<th align="left" bgcolor="#eeeeec">Function</th>
+<th align="left" bgcolor="#eeeeec">Location</th>
+</tr>
+<tr>
+<td bgcolor="#eeeeec" align="center">1</td>
+<td bgcolor="#eeeeec" align="center">0.0013</td>
+<td bgcolor="#eeeeec" align="right">364040</td>
+<td bgcolor="#eeeeec">{main}(  )</td>
+<td title="C:\wamp86\www\wordpress\wp-content\plugins\wpforms-lite\vendor\goodby\csv\src\Goodby\CSV\Export\Standard\Collection\PdoCollection.php" bgcolor="#eeeeec">...\PdoCollection.php<b>:</b>0</td>
+</tr>
+</table></font>
+<br>
+<font size="1"><table class="xdebug-error xe-deprecated" dir="ltr" border="1" cellspacing="0" cellpadding="1">
+<tr><th align="left" bgcolor="#f57900" colspan="5">
+<span style="background-color: #cc0000; color: #fce94f; font-size: x-large;">( ! )</span> Deprecated: Return type of Goodby\CSV\Export\Standard\Collection\PdoCollection::rewind() should either be compatible with Iterator::rewind(): void, or the #[\ReturnTypeWillChange] attribute should be used to temporarily suppress the notice in C:\wamp86\www\wordpress\wp-content\plugins\wpforms-lite\vendor\goodby\csv\src\Goodby\CSV\Export\Standard\Collection\PdoCollection.php on line <i>77</i>
+</th></tr>
+<tr><th align="left" bgcolor="#e9b96e" colspan="5">Call Stack</th></tr>
+<tr>
+<th align="center" bgcolor="#eeeeec">#</th>
+<th align="left" bgcolor="#eeeeec">Time</th>
+<th align="left" bgcolor="#eeeeec">Memory</th>
+<th align="left" bgcolor="#eeeeec">Function</th>
+<th align="left" bgcolor="#eeeeec">Location</th>
+</tr>
+<tr>
+<td bgcolor="#eeeeec" align="center">1</td>
+<td bgcolor="#eeeeec" align="center">0.0013</td>
+<td bgcolor="#eeeeec" align="right">364040</td>
+<td bgcolor="#eeeeec">{main}(  )</td>
+<td title="C:\wamp86\www\wordpress\wp-content\plugins\wpforms-lite\vendor\goodby\csv\src\Goodby\CSV\Export\Standard\Collection\PdoCollection.php" bgcolor="#eeeeec">...\PdoCollection.php<b>:</b>0</td>
+</tr>
+</table></font>
