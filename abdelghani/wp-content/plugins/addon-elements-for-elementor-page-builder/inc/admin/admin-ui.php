119a20750b58a71cbb7d387498b22733601dcfc9
haniMBT <abdelghaani16@gmail.com>
2023-12-21 12:05:21 +0100

    new repo abdleghani

---
+++ abdelghani/wp-content/plugins/addon-elements-for-elementor-page-builder/inc/admin/admin-ui.php
@@ -0,0 +1,31 @@
+<br>
+<font size="1"><table class="xdebug-error xe-uncaught-exception" dir="ltr" border="1" cellspacing="0" cellpadding="1">
+<tr><th align="left" bgcolor="#f57900" colspan="5">
+<span style="background-color: #cc0000; color: #fce94f; font-size: x-large;">( ! )</span> Fatal error: Uncaught Error: Call to undefined function WTS_EAE\add_action() in C:\wamp86\www\wordpress\wp-content\plugins\addon-elements-for-elementor-page-builder\inc\admin\admin-ui.php on line <i>24</i>
+</th></tr>
+<tr><th align="left" bgcolor="#f57900" colspan="5">
+<span style="background-color: #cc0000; color: #fce94f; font-size: x-large;">( ! )</span> Error: Call to undefined function WTS_EAE\add_action() in C:\wamp86\www\wordpress\wp-content\plugins\addon-elements-for-elementor-page-builder\inc\admin\admin-ui.php on line <i>24</i>
+</th></tr>
+<tr><th align="left" bgcolor="#e9b96e" colspan="5">Call Stack</th></tr>
+<tr>
+<th align="center" bgcolor="#eeeeec">#</th>
+<th align="left" bgcolor="#eeeeec">Time</th>
+<th align="left" bgcolor="#eeeeec">Memory</th>
+<th align="left" bgcolor="#eeeeec">Function</th>
+<th align="left" bgcolor="#eeeeec">Location</th>
+</tr>
+<tr>
+<td bgcolor="#eeeeec" align="center">1</td>
+<td bgcolor="#eeeeec" align="center">0.0002</td>
+<td bgcolor="#eeeeec" align="right">355896</td>
+<td bgcolor="#eeeeec">{main}(  )</td>
+<td title="C:\wamp86\www\wordpress\wp-content\plugins\addon-elements-for-elementor-page-builder\inc\admin\admin-ui.php" bgcolor="#eeeeec">...\admin-ui.php<b>:</b>0</td>
+</tr>
+<tr>
+<td bgcolor="#eeeeec" align="center">2</td>
+<td bgcolor="#eeeeec" align="center">0.0002</td>
+<td bgcolor="#eeeeec" align="right">355976</td>
+<td bgcolor="#eeeeec">WTS_EAE\Admin_Ui->__construct(  )</td>
+<td title="C:\wamp86\www\wordpress\wp-content\plugins\addon-elements-for-elementor-page-builder\inc\admin\admin-ui.php" bgcolor="#eeeeec">...\admin-ui.php<b>:</b>491</td>
+</tr>
+</table></font>
