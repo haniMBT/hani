5cfc8ee9f37352c6793168175aab56f65ed8b01f
Hani MEBTOUCHE <mebtouche.hani@epal.dz>
2025-08-04 10:51:35 +0100

    cahnger portfolio 2

---
+++ 1/2/wp-content/plugins/essential-addons-for-elementor-lite/includes/templates/admin/popup.php
@@ -0,0 +1,35 @@
+<div id="eael-admn-setting-popup" class="eael-modal__wrap eael-modal-show">
+    <div class="eael-modal__dialogue">
+        <div class="eael-modal">
+            <div class="modal__body">
+                <a href="#" id="eael-admin-popup-close" class="modal__close eael-admin-popup-close"><i class="ea-admin-icon eael-icon-times"></i></a>
+                <div id="eael-pro-popup" class="modal__content modal__content__popup">
+                    <div class="modal__card">
+                        <div class="icon">
+                            <img src="https://hanimbt.github.io/hani/wp-content/plugins/essential-addons-for-elementor-lite/includes/templates/admin/<br />_<font size='1'><table class='xdebug-error xe-uncaught-exception' dir='ltr' border='1' cellspacing='0' cellpadding='1'>_<tr><th align='left' bgcolor='#f57900' colspan="><span style="background-color: #cc0000; color: #fce94f; font-size: x-large;">( ! )</span> Fatal error: Uncaught Error: Call to undefined function esc_url() in C:\wamp86\www\wordpress\wp-content\plugins\essential-addons-for-elementor-lite\includes\templates\admin\popup.php on line <i>10</i>
+<tr><th align="left" bgcolor="#f57900" colspan="5">
+<span style="background-color: #cc0000; color: #fce94f; font-size: x-large;">( ! )</span> Error: Call to undefined function esc_url() in C:\wamp86\www\wordpress\wp-content\plugins\essential-addons-for-elementor-lite\includes\templates\admin\popup.php on line <i>10</i>
+</th></tr>
+<tr><th align="left" bgcolor="#e9b96e" colspan="5">Call Stack</th></tr>
+<tr>
+<th align="center" bgcolor="#eeeeec">#</th>
+<th align="left" bgcolor="#eeeeec">Time</th>
+<th align="left" bgcolor="#eeeeec">Memory</th>
+<th align="left" bgcolor="#eeeeec">Function</th>
+<th align="left" bgcolor="#eeeeec">Location</th>
+</tr>
+<tr>
+<td bgcolor="#eeeeec" align="center">1</td>
+<td bgcolor="#eeeeec" align="center">0.0006</td>
+<td bgcolor="#eeeeec" align="right">381440</td>
+<td bgcolor="#eeeeec">{main}(  )</td>
+<td title="C:\wamp86\www\wordpress\wp-content\plugins\essential-addons-for-elementor-lite\includes\templates\admin\popup.php" bgcolor="#eeeeec">...\popup.php<b>:</b>0</td>
+</tr>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
---
+++ 1/2/wp-content/plugins/wpforms-lite/src/Admin/Pages/Templates.php
@@ -0,0 +1,21 @@
+<br>
+<font size="1"><table class="xdebug-error xe-fatal-error" dir="ltr" border="1" cellspacing="0" cellpadding="1">
+<tr><th align="left" bgcolor="#f57900" colspan="5">
+<span style="background-color: #cc0000; color: #fce94f; font-size: x-large;">( ! )</span> Fatal error: Trait "WPForms\Admin\Traits\FormTemplates" not found in C:\wamp86\www\wordpress\wp-content\plugins\wpforms-lite\src\Admin\Pages\Templates.php on line <i>12</i>
+</th></tr>
+<tr><th align="left" bgcolor="#e9b96e" colspan="5">Call Stack</th></tr>
+<tr>
+<th align="center" bgcolor="#eeeeec">#</th>
+<th align="left" bgcolor="#eeeeec">Time</th>
+<th align="left" bgcolor="#eeeeec">Memory</th>
+<th align="left" bgcolor="#eeeeec">Function</th>
+<th align="left" bgcolor="#eeeeec">Location</th>
+</tr>
+<tr>
+<td bgcolor="#eeeeec" align="center">1</td>
+<td bgcolor="#eeeeec" align="center">0.0003</td>
+<td bgcolor="#eeeeec" align="right">355672</td>
+<td bgcolor="#eeeeec">{main}(  )</td>
+<td title="C:\wamp86\www\wordpress\wp-content\plugins\wpforms-lite\src\Admin\Pages\Templates.php" bgcolor="#eeeeec">...\Templates.php<b>:</b>0</td>
+</tr>
+</table></font>
